d8c0b1a6cfb26ceadd297b65a6e047309fa4df00
Gino Canessa <ginocanessa@gmail.com>
2020-07-07 11:25:03 -0500

    Adding documentation for Pages

--- src/fhir-codegen-cli/GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-﻿// -------------------------------------------------------------------------------------------------
-// <copyright file="GlobalSuppressions.cs" company="Microsoft Corporation">
-//     Copyright (c) Microsoft Corporation. All rights reserved.
-//     Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
-// </copyright>
-// -------------------------------------------------------------------------------------------------
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1309:Field names should not begin with underscore", Justification = "Reviewed")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1101:Prefix local calls with this", Justification = "Reviewed")]
