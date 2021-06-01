a5effce1180bbb55279aab02e806ac18c1928097
Gino Canessa <gino.canessa@microsoft.com>
2021-06-01 11:56:54 -0500

    Further updates to directory searching

--- src/fhir-codegen-cli/Program.cs
+++ src/fhir-codegen-cli/Program.cs
@@ -91,7 +91,7 @@ namespace FhirCodegenCli
 
             if (string.IsNullOrEmpty(fhirSpecDirectory))
             {
-                fhirSpecDirectory = Path.Combine(currentFilePath, "..", "..", "..", "..", "..", "fhirVersions");
+                fhirSpecDirectory = FindFhirDir(currentFilePath, "fhirVersions");
             }
 
             if (!string.IsNullOrEmpty(loadLocalFhirBuild))
@@ -108,7 +108,7 @@ namespace FhirCodegenCli
 
             if (string.IsNullOrEmpty(outputPath))
             {
-                outputPath = Path.Combine(currentFilePath, "..", "..", "..", "..", "..", "generated");
+                outputPath = FindFhirDir(currentFilePath, "generated");
             }
 
             if (string.IsNullOrEmpty(language))
@@ -372,6 +372,32 @@ namespace FhirCodegenCli
             {
                 Console.WriteLine($"+ {file}");
             }
+        }
+
+        /// <summary>Searches for the FHIR specification directory.</summary>
+        /// <exception cref="DirectoryNotFoundException">Thrown when the requested directory is not
+        ///  present.</exception>
+        /// <param name="startingDir">The starting dir.</param>
+        /// <param name="dirName">    The name of the directory we are searching for.</param>
+        /// <returns>The found FHIR directory.</returns>
+        public static string FindFhirDir(string startingDir, string dirName)
+        {
+            string currentDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string testDir = Path.Combine(currentDir, dirName);
+
+            while (!Directory.Exists(testDir))
+            {
+                currentDir = Path.GetFullPath(Path.Combine(currentDir, ".."));
+
+                if (currentDir == Path.GetPathRoot(currentDir))
+                {
+                    throw new DirectoryNotFoundException("Could not find spec directory in path!");
+                }
+
+                testDir = Path.Combine(currentDir, dirName);
+            }
+
+            return testDir;
         }
 
         /// <summary>Shows the language help.</summary>
