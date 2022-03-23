c5f95a66187bb573b6471c2211cedfdcdb0e50da
Gino Canessa <gino.canessa@microsoft.com>
2022-03-23 10:37:22 -0500

    Do not recurse into Extensions for display. Added breadcrumb nav to header. Better toggling of sidebar nav. Added rendering of Markdown in Element comments. Slicing improvements.

---
+++ src/FhirCodeGenBlazor/Models/INavTracker.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="INavTracker.cs" company="Microsoft Corporation">
+//     Copyright (c) Microsoft Corporation. All rights reserved.
+//     Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// </copyright>
+
+namespace FhirCodeGenBlazor.Models;
+
+/// <summary>Interface for navigation tracker.</summary>
+public interface INavTracker
+{
+    /// <summary>Notifies a navigation.</summary>
+    /// <param name="page"> The page.</param>
+    /// <param name="link"> The link.</param>
+    /// <param name="depth">The depth.</param>
+    void NotifyNav(string page, string link, int depth);
+}
