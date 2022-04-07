9922bc4e03c7cebf872710a39914e2e11953b8c6
Gino Canessa <gino.canessa@microsoft.com>
2022-04-07 17:00:53 -0500

    TS2 WIP

--- languageInput/TypeScript2/dynamic/ClassHumanName.ts
+++ languageInput/TypeScript2/dynamic/ClassHumanName.ts
@@ -1,50 +1,50 @@
 
-/**
- * Convert a HumanName into a displayable string
- */
-toDisplay(familyFirst:boolean = true, includeAnnotations:boolean = false):string {
-  if ((this.text) && (this.text.length > 0)) {
-    return this.text;
-  }
+  /**
+   * Convert a HumanName into a displayable string
+   */
+  toDisplay(familyFirst:boolean = true, includeAnnotations:boolean = false):string {
+    if ((this.text) && (this.text.length > 0)) {
+      return this.text;
+    }
 
-  var val:string = '';
+    var val:string = '';
 
-  if (familyFirst) {
-    if (this.family) {
-      val = this.family;
+    if (familyFirst) {
+      if (this.family) {
+        val = this.family;
+      }
+
+      if (this.given) {
+        val += (val.length > 0 ? ', ' : '') + this.given.join(' ');
+      }
+
+      if (includeAnnotations) {
+        if (this.suffix) {
+          val += (val.length > 0 ? ', ' : '') + this.suffix.join(', ');
+        }
+
+        if (this.prefix) {
+          val += (val.length > 0 ? ', ' : '') + this.prefix.join(', ');
+        }
+      }
+
+      return val;
+    }
+
+    if ((includeAnnotations) && (this.prefix)) {
+      val += this.prefix.join(', ');
     }
 
     if (this.given) {
-      val += (val.length > 0 ? ', ' : '') + this.given.join(' ');
+      val = (val.length > 0 ? ' ' : '') + this.given.join(' ');
+    }
+    if (this.family) {
+      val += (val.length > 0 ? ' ' : '') + this.family;
     }
 
-    if (includeAnnotations) {
-      if (this.suffix) {
-        val += (val.length > 0 ? ', ' : '') + this.suffix.join(', ');
-      }
-
-      if (this.prefix) {
-        val += (val.length > 0 ? ', ' : '') + this.prefix.join(', ');
-      }
+    if ((includeAnnotations) && (this.suffix)) {
+      val += (val.length > 0 ? ', ' : '') + this.suffix.join(', ');
     }
 
     return val;
   }
-
-  if ((includeAnnotations) && (this.prefix)) {
-    val += this.prefix.join(', ');
-  }
-
-  if (this.given) {
-    val = (val.length > 0 ? ' ' : '') + this.given.join(' ');
-  }
-  if (this.family) {
-    val += (val.length > 0 ? ' ' : '') + this.family;
-  }
-
-  if ((includeAnnotations) && (this.suffix)) {
-    val += (val.length > 0 ? ', ' : '') + this.suffix.join(', ');
-  }
-
-  return val;
-}
