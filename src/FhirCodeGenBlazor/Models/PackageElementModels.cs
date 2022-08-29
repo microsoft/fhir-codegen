b8851659ff9c502302768f66c891bdf1a32103f7
Gino Canessa <gino.canessa@microsoft.com>
2022-08-29 12:53:07 -0500

    First pass of paging in complex search.

---
+++ src/FhirCodeGenBlazor/Models/PackageElementModels.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="PackageElementModels.cs" company="Microsoft Corporation">
+//     Copyright (c) Microsoft Corporation. All rights reserved.
+//     Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// </copyright>
+
+using Microsoft.Health.Fhir.CodeGenCommon.Models;
+
+namespace FhirCodeGenBlazor.Models;
+
+/// <summary>A package element models.</summary>
+public abstract class PackageElementModels
+{
+    /// <summary>Information about the element.</summary>
+    public readonly record struct ElementRecord(
+        FhirArtifactClassEnum DefinedByClass,
+        FhirComplex RootComplex,
+        FhirElement Element);
+}
