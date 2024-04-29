abca0ba7fe9f2367e41282ef375a27b7345a1130
Gino Canessa <gino.canessa@microsoft.com>
2024-04-28 20:50:41 -0500

    WIP checkpoint - working on comparison.

--- src/Microsoft.Health.Fhir.CodeGen/CompareTool/PackageComparer.cs
+++ src/Microsoft.Health.Fhir.CodeGen/CompareTool/PackageComparer.cs
@@ -5,8 +5,12 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Hl7.Fhir.Model;
 using Microsoft.Health.Fhir.CodeGen.Configuration;
@@ -23,11 +27,89 @@ public class PackageComparer
 
     ConfigCompare _config;
 
+    HttpClient? _httpClient = null;
+    Uri? _ollamaUri = null;
+
+    public record class PackagePathRenames
+    {
+        public required string PackageDirectiveLeft { get; init; }
+        public required string PackageDirectiveRight { get; init; }
+        public required Dictionary<string, string> LeftRightPath { get; init; }
+    }
+
+    public record class ValueSetInfoRec
+    {
+        public required string Url { get; init; }
+        public required string Name { get; init; }
+    }
+
+    public record class ElementTypeInfoRec
+    {
+        public required string Name { get; init; }
+        public required List<string> Profiles { get; init; }
+        public required List<string> TargetProfiles { get; init; }
+    }
+
+    public record class ElementInfoRec
+    {
+        public required string Name { get; init; }
+        public required string Path { get; init; }
+        public required string Short { get; init; }
+        public required string Description { get; init; }
+
+        public required int MinCardinality { get; init; }
+        public required int MaxCardinality { get; init; }
+
+        public required string BindingStrength { get; init; }
+
+        public required string BindingValueSet { get; init; }
+
+        public required Dictionary<string, ElementTypeInfoRec> ElementTypes { get; init; }
+    }
+
+    public record class StructureInfoRec
+    {
+        public required string Name { get; init; }
+        public required string Title { get; init; }
+        public required string Description { get; init; }
+        public required string Purpose { get; init; }
+    }
+
+    public record class PackageComparison
+    {
+        public required string LeftPackageId { get; init; }
+        public required string LeftPackageVersion { get; init; }
+        public required string RightPackageId { get; init; }
+        public required string RightPackageVersion { get; init; }
+
+        public required Dictionary<string, ComparisonRecord<StructureInfoRec>> PrimitiveTypes { get; init; }
+    }
+
+    public class ComparisonRecord<T>
+    {
+        public required T? Left { get; init; }
+        public required T? Right { get; init; }
+
+        public required bool Match { get; init; }
+
+        public T? AiPrediction { get; init; }
+        public int AiConfidence { get; init; } = 0;
+    }
+
     public PackageComparer(ConfigCompare config, DefinitionCollection left, DefinitionCollection right)
     {
         _config = config;
         _left = left;
         _right = right;
+
+        if (!string.IsNullOrEmpty(config.OllamaUrl) &&
+            !string.IsNullOrEmpty(config.OllamaModel))
+        {
+            _httpClient = new HttpClient();
+            _ollamaUri = config.OllamaUrl.EndsWith("generate", StringComparison.OrdinalIgnoreCase)
+                ? new Uri(config.OllamaUrl)
+                : new Uri(new Uri(config.OllamaUrl), "api/generate");
+        }
     }
 
     public void Compare()
@@ -44,23 +126,161 @@ public class PackageComparer
 
         using ExportStreamWriter mdWriter = new(mdFullFilename);
 
-        ComparePrimitiveTypes(mdWriter, _left.PrimitiveTypesByName, _right.PrimitiveTypesByName);
+        Dictionary<string, ComparisonRecord<StructureInfoRec>> primitives = CompareStructures(mdWriter, _left.PrimitiveTypesByName, _right.PrimitiveTypesByName);
 
     }
 
-    private void ComparePrimitiveTypes(
-        ExportStreamWriter mdWriter,
-        IReadOnlyDictionary<string, StructureDefinition> A,
-        IReadOnlyDictionary<string, StructureDefinition> B)
+    private void LoadKnownChanges()
     {
-        HashSet<string> keysA = A?.Keys.ToHashSet() ?? [];
-        HashSet<string> keysB = B?.Keys.ToHashSet() ?? [];
+        // TODO(ginoc): implement
+    }
 
-        HashSet<string> keyIntersection = A?.Keys.ToHashSet() ?? [];
-        keyIntersection.IntersectWith(keysB);
+    private Dictionary<string, ComparisonRecord<StructureInfoRec>> CompareStructures(
+        ExportStreamWriter mdWriter,
+        IReadOnlyDictionary<string, StructureDefinition> leftStructures,
+        IReadOnlyDictionary<string, StructureDefinition> rightStructures)
+    {
+        Dictionary<string, StructureInfoRec> left = leftStructures.ToDictionary(kvp => kvp.Key, kvp => GetInfo(kvp.Value));
+        Dictionary<string, StructureInfoRec> right = rightStructures.ToDictionary(kvp => kvp.Key, kvp => GetInfo(kvp.Value));
 
-        keysA.ExceptWith(keyIntersection);
-        keysB.ExceptWith(keyIntersection);
+        Dictionary<string, ComparisonRecord<StructureInfoRec>> comparsion = [];
+
+        HashSet<string> keysLeft = left.Keys.ToHashSet() ?? [];
+        HashSet<string> keysRight = right.Keys.ToHashSet() ?? [];
+
+        HashSet<string> keyIntersection = left.Keys.ToHashSet() ?? [];
+        keyIntersection.IntersectWith(keysRight);
+
+        keysLeft.ExceptWith(keyIntersection);
+        keysRight.ExceptWith(keyIntersection);
+
+        // add our matches
+        foreach (string key in keyIntersection)
+        {
+            comparsion.Add(key, new()
+            {
+                Left = left[key],
+                Right = right[key],
+                Match = true,
+            });
+        }
+
+        // traverse keys in left that do not exist in right
+        foreach (string key in keysLeft)
+        {
+            // 
+        }
+
+        return comparsion;
+    }
+
+    private StructureInfoRec GetInfo(StructureDefinition sd)
+    {
+        return new StructureInfoRec()
+        {
+            Name = sd.Name,
+            Title = sd.Title,
+            Description = sd.Description,
+            Purpose = sd.Purpose,
+        };
+    }
+
+    private class OllamaQuery
+    {
+        [JsonPropertyName("model")]
+        public required string Model { get; set; }
+
+        [JsonPropertyName("prompt")]
+        public required string Prompt { get; set; }
+
+        [JsonPropertyName("format")]
+        public string Format { get; set; } = "json";
+
+        [JsonPropertyName("stream")]
+        public bool Stream = false;
+    }
+
+
+    public class OllamaResponse
+    {
+        [JsonPropertyName("model")]
+        public string Model { get; set; } = string.Empty;
+
+        [JsonPropertyName("created_at")]
+        public DateTime CreatedAt { get; set; } = DateTime.MinValue;
+
+        [JsonPropertyName("response")]
+        public string Response { get; set; } = string.Empty;
+
+        [JsonPropertyName("done")]
+        public bool Done { get; set; } = false;
+
+        [JsonPropertyName("context")]
+        public int[] Context { get; set; } = [];
+
+        [JsonPropertyName("total_duration")]
+        public long TotalDuration { get; set; } = 0;
+
+        [JsonPropertyName("load_duration")]
+        public int LoadDuration { get; set; } = 0;
+
+        [JsonPropertyName("prompt_eval_count")]
+        public int PromptEvalCount { get; set; } = 0;
+
+        [JsonPropertyName("prompt_eval_duration")]
+        public int PromptEvalDuration { get; set; } = 0;
+
+        [JsonPropertyName("eval_count")]
+        public int EvalCount { get; set; } = 0;
+
+        [JsonPropertyName("eval_duration")]
+        public long EvalDuration { get; set; } = 0;
+    }
+
+
+    private bool TryAskOllama(
+        StructureInfoRec known,
+        IEnumerable<StructureInfoRec> possibles,
+        [NotNullWhen(true)] out StructureInfoRec? guess,
+        [NotNullWhen(true)] out int? confidence)
+    {
+        if (_httpClient == null)
+        {
+            guess = null;
+            confidence = 0;
+            return false;
+        }
+
+        string prompt =
+            $"Given the following definition: {JsonSerializer.Serialize(known)}," +
+            $" please select the most likely match from the following definitions: {JsonSerializer.Serialize(possibles)}." +
+            $" Respond in JSON, keeping the definition in the same format.";
+
+        // build our prompt
+        OllamaQuery query = new()
+        {
+            Model = _config.OllamaModel,
+            Prompt = prompt,
+        };
+
+        HttpRequestMessage request = new HttpRequestMessage()
+        {
+            Method = HttpMethod.Post,
+            RequestUri = _ollamaUri,
+            Headers =
+            {
+                Accept =
+                {
+                    new MediaTypeWithQualityHeaderValue("application/json"),
+                },
+            },
+        };
+
+        guess = null;
+        confidence = 0;
+        return false;
+
+
     }
 
     private string SanitizeVersion(string version)
--- src/Microsoft.Health.Fhir.CodeGen/Configuration/ConfigCompare.cs
+++ src/Microsoft.Health.Fhir.CodeGen/Configuration/ConfigCompare.cs
@@ -31,9 +31,66 @@ public class ConfigCompare : ConfigRoot
         },
     };
 
+    [ConfigOption(
+        ArgName = "--known-change-path",
+        EnvName = "Known_Change_Path",
+        ArgArity = "0..1",
+        Description = "Source path for known version changes.")]
+    public string KnownChangePath { get; set; } = "./renames";
+
+    private static ConfigurationOption KnownChangePathParameter { get; } = new()
+    {
+        Name = "Known_Change_Path",
+        DefaultValue = "./known-changes",
+        CliOption = new System.CommandLine.Option<string[]>("--known-change-path", "Source path for known version changes.")
+        {
+            Arity = System.CommandLine.ArgumentArity.ZeroOrOne,
+            IsRequired = false,
+        },
+    };
+
+    [ConfigOption(
+        ArgName = "--ollama-url",
+        EnvName = "Ollama_Url",
+        ArgArity = "0..1",
+        Description = "Base URL for Ollama evaluation.")]
+    public string OllamaUrl { get; set; } = string.Empty;
+
+    private static ConfigurationOption OllamaUrlParameter { get; } = new()
+    {
+        Name = "Ollama_Url",
+        DefaultValue = string.Empty,
+        CliOption = new System.CommandLine.Option<string[]>("--ollama-url", "Base URL for Ollama evaluation.")
+        {
+            Arity = System.CommandLine.ArgumentArity.ZeroOrOne,
+            IsRequired = false,
+        },
+    };
+
+    [ConfigOption(
+        ArgName = "--ollama-model",
+        EnvName = "Ollama_Model",
+        ArgArity = "0..1",
+        Description = "Model name for Ollama evaluation.")]
+    public string OllamaModel { get; set; } = string.Empty;
+    private static ConfigurationOption OllamaModelParameter { get; } = new()
+    {
+        Name = "Ollama_Model",
+        DefaultValue = string.Empty,
+        CliOption = new System.CommandLine.Option<string[]>("--ollama-model", "Model name for Ollama evaluation.")
+        {
+            Arity = System.CommandLine.ArgumentArity.ZeroOrOne,
+            IsRequired = false,
+        },
+    };
+
+
     private static readonly ConfigurationOption[] _options =
     [
         ComparePackagesParameter,
+        KnownChangePathParameter,
+        OllamaUrlParameter,
+        OllamaModelParameter,
     ];
 
     /// <summary>Gets the array of configuration options.</summary>
@@ -55,6 +112,15 @@ public class ConfigCompare : ConfigRoot
             {
                 case "Compare_Package":
                     ComparePackages = GetOptArray(parseResult, opt.CliOption, ComparePackages);
+                    break;
+                case "Known_Change_Path":
+                    KnownChangePath = GetOpt(parseResult, opt.CliOption, KnownChangePath);
+                    break;
+                case "Ollama_Url":
+                    OllamaUrl = GetOpt(parseResult, opt.CliOption, OllamaUrl);
+                    break;
+                case "Ollama_Model":
+                    OllamaModel = GetOpt(parseResult, opt.CliOption, OllamaModel);
                     break;
             }
         }
