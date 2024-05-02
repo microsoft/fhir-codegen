142494ed0a2a0260bb4ce73fc0d98edea7e2c54d
Gino Canessa <gino.canessa@microsoft.com>
2024-05-01 20:06:58 -0500

    WIP - cross version interactive work.

--- src/fhir-codegen/CrossVersionInteractive.cs
+++ src/fhir-codegen/CrossVersionInteractive.cs
@@ -58,17 +58,23 @@ internal class CrossVersionInteractive
         Done,
     }
 
-    private UiStateCodes _state = UiStateCodes.SelectingPackages;
-    private bool _stateChanged = true;
+    private UiStateCodes _state = UiStateCodes.Main;
 
     private List<FhirReleases.PublishedReleaseInformation> _releases = FhirReleases.FhirPublishedVersions.Values.Where(i => i.IsSequenceOfficial).OrderBy(i => i.Sequence).ToList();
 
     private FhirReleases.PublishedReleaseInformation? _left = null;
     private FhirReleases.PublishedReleaseInformation? _right = null;
 
+    private bool _packagesLoaded = false;
+
+    private int _topY = 0;
+
+    private System.Timers.Timer? _uiRefreshTimer = null;
+
     private void Run()
     {
         Application.Init();
+
 
         //MenuBar menu = new()
         //{
@@ -94,6 +100,12 @@ internal class CrossVersionInteractive
 
         while ((_state != UiStateCodes.Done) && (_state != UiStateCodes.Unknown))
         {
+            if (ConfigurationManager.Themes != null)
+            {
+                ConfigurationManager.Themes.Theme = "Default";
+                ConfigurationManager.Apply();
+            }
+
             Window mainWindow = new()
             {
                 Title = "FHIR Cross Version Review (Ctrl+Q to quit)",
@@ -128,11 +140,15 @@ internal class CrossVersionInteractive
 
             AddUiForState(current, contentWindow);
             mainWindow.Add(contentWindow);
+            //AddUiForState(current, mainWindow);
 
             Application.Run(mainWindow);
             mainWindow.Dispose();
             Application.Shutdown();
         }
+
+        _uiRefreshTimer?.Stop();
+        _uiRefreshTimer?.Dispose();
     }
 
 
@@ -142,92 +158,18 @@ internal class CrossVersionInteractive
         {
             case UiStateCodes.Main:
                 {
-                    if ((_left is null) || (_right is null))
-                    {
-                        Label label = new()
-                        {
-                            X = 0,
-                            Y = 0,
-                            Width = Dim.Percent(100),
-                            Text = "Please select packages...",
-                        };
-                        w.Add(label);
-
-                        Button selectButton = new()
-                        {
-                            Text = "_Select Packages",
-                            X = 0,
-                            Y = Pos.Bottom(label) + 1,
-                        };
-
-                        selectButton.Accept += (s, e) =>
-                        {
-                            _state = UiStateCodes.SelectingPackages;
-                            Application.RequestStop();
-                        };
-
-                        //selectButton.Accept += (CancelEventArgs e) =>
-                        //{
-                        //    _state = UiStateCodes.SelectingPackages;
-                        //    Application.RequestStop();
-                        //};
-                        w.Add(selectButton);
-
-                        return;
-                    }
-
-                    Label labelLeft = new()
-                    {
-                        X = 0,
-                        Y = 0,
-                        Width = Dim.Percent(100),
-                        Text = $" Left package: hl7.fhir.{_left?.Sequence.ToRLiteral().ToLowerInvariant()}.core#{_left?.Version}",
-                    };
-                    w.Add(labelLeft);
-
-                    Label labelRight = new()
-                    {
-                        X = 0,
-                        Y = Pos.Bottom(labelLeft) + 1,
-                        Width = Dim.Percent(100),
-                        Text = $"Right package: hl7.fhir.{_right?.Sequence.ToRLiteral().ToLowerInvariant()}.core#{_right?.Version}",
-                    };
-                    w.Add(labelRight);
-
+                    AddMainUi(w);
                     _state = UiStateCodes.Main;
                 }
                 break;
 
             case UiStateCodes.SelectingPackages:
                 {
+                    _packagesLoaded = false;
+
                     try
                     {
-                        Button cancelButton = new()
-                        {
-                            Text = "_Cancel",
-                            X = 0,
-                            Y = 0,
-                        };
-
-                        Dialog d = new()
-                        {
-                            Title = "Select Packages...",
-                            Buttons = [ cancelButton ],
-                            Width = Dim.Fill(),
-                            Height = Dim.Fill(),
-                            ButtonAlignment = Dialog.ButtonAlignments.Center,
-                        };
-
-                        cancelButton.Accept += (s, e) =>
-                        {
-                            _left = null;
-                            _right = null;
-                            Application.RequestStop(d);
-                        };
-
-                        ConfigureSelectionUi(d);
-
-                        w.Add(d);
+                        w.Add(BuildSelectionUi());
                     }
                     catch (Exception)
                     {
@@ -237,15 +179,10 @@ internal class CrossVersionInteractive
                     _state = (_left is null || _right is null) ? UiStateCodes.Main : UiStateCodes.LoadingPackages;
                 }
                 break;
+
             case UiStateCodes.LoadingPackages:
                 {
-                    //VersionSelectionDialog win = new();
-                    //Application.Top.Add(menu, win);
-                    //Application.Run();
-                    //Application.Shutdown();
-
-                    //_state = UiStateCodes.Done;
-
+                    AddLoadingUi(w);
                     _state = UiStateCodes.LoadingPackages;
                 }
                 break;
@@ -257,75 +194,281 @@ internal class CrossVersionInteractive
         }
     }
 
-    private void ConfigureSelectionUi(Window w)
+    private void AddLoadingUi(Window w)
     {
-        w.Title = "Select packages for crossing...";
-
-        w.X = 0;
-        w.Y = 0;
-        w.Width = Dim.Fill();
-        w.Height = Dim.Fill();
-
-        Label labelSelectedLeft = new()
+        Label labelLeft = new()
         {
             X = 0,
-            Y = 0,
-            Width = Dim.Percent(50),
-            Text = _left?.Description ?? "Select Left Version",
+            Y = _topY,
+            Width = Dim.Percent(100),
+            Height = 1,
+            Text = $" Left package: hl7.fhir.{_left?.Sequence.ToRLiteral().ToLowerInvariant()}.core#{_left?.Version}",
         };
-        w.Add(labelSelectedLeft);
+        w.Add(labelLeft);
+
+        Label labelRight = new()
+        {
+            X = 0,
+            Y = Pos.Bottom(labelLeft),
+            Width = Dim.Percent(100),
+            Height = 1,
+            Text = $"Right package: hl7.fhir.{_right?.Sequence.ToRLiteral().ToLowerInvariant()}.core#{_right?.Version}",
+        };
+        w.Add(labelRight);
+
+        Label labelLoading = new()
+        {
+            X = 0,
+            Y = Pos.Bottom(labelRight) + 2,
+            Width = 10,
+            Height = 1,
+            Text = "Loading..."
+        };
+        w.Add(labelLoading);
+
+        SpinnerView spinnerView = new SpinnerView()
+        {
+            X = Pos.Right(labelLoading),
+            Y = Pos.Top(labelLoading),
+            Width = 10,
+            Height = 1,
+            AutoSpin = true,
+            SpinDelay = 125,
+            SpinBounce = true,
+            Visible = true,
+            Style = new SpinnerStyle.Points(),
+        };
+        w.Add(spinnerView);
+        //spinnerView.AutoSpin = true;
+    }
+
+    private void AddMainUi(Window w)
+    {
+        if ((_left is null) || (_right is null))
+        {
+            Button selectButton = new()
+            {
+                Text = "Select _Packages",
+                X = 0,
+                Y = _topY,
+            };
+
+            selectButton.Accept += (s, e) =>
+            {
+                _state = UiStateCodes.SelectingPackages;
+                Application.RequestStop();
+            };
+
+            w.Add(selectButton);
+
+            return;
+        }
+
+        Label labelLeft = new()
+        {
+            X = 0,
+            Y = _topY,
+            Width = Dim.Percent(100),
+            Height = 1,
+            Text = $" Left package: hl7.fhir.{_left?.Sequence.ToRLiteral().ToLowerInvariant()}.core#{_left?.Version}",
+        };
+        w.Add(labelLeft);
+
+        Label labelRight = new()
+        {
+            X = 0,
+            Y = Pos.Bottom(labelLeft),
+            Width = Dim.Percent(100),
+            Height = 1,
+            Text = $"Right package: hl7.fhir.{_right?.Sequence.ToRLiteral().ToLowerInvariant()}.core#{_right?.Version}",
+        };
+        w.Add(labelRight);
+
+        Button buttonLoad = new()
+        {
+            X = 0,
+            Y = Pos.Bottom(labelRight),
+            Height = 1,
+            Text = "_Load Packages",
+        };
+
+        Label labelLoading = new()
+        {
+            X = 0,
+            Y = Pos.Bottom(labelRight),
+            Width = 10,
+            Height = 1,
+            Text = "Loading...",
+            Visible = false,
+        };
+        w.Add(labelLoading);
+
+        SpinnerView spinnerView = new SpinnerView()
+        {
+            X = Pos.Right(labelLoading),
+            Y = Pos.Top(labelLoading),
+            Width = 10,
+            Height = 1,
+            AutoSpin = false,
+            SpinDelay = 125,
+            SpinBounce = true,
+            Visible = false,
+            Style = new SpinnerStyle.Points(),
+        };
+        w.Add(spinnerView);
+
+        buttonLoad.Accept += (s, e) =>
+        {
+            buttonLoad.Visible = false;
+            labelLoading.Visible = true;
+            spinnerView.Visible = true;
+
+            //_uiRefreshTimer = new()
+            //{
+            //    AutoReset = true,
+            //    Interval = 150,
+            //};
+            //_uiRefreshTimer.Elapsed += (s, e) => { spinnerView. };
+
+            w.SetNeedsDisplay();
+
+            //_state = UiStateCodes.LoadingPackages;
+            //Application.RequestStop();
+        };
+
+        w.Add(buttonLoad);
+    }
+
+    private Dialog BuildSelectionUi()
+    {
+        Button cancelButton = new()
+        {
+            Text = "_Cancel",
+            //X = 0,
+            //Y = 0,
+        };
+
+        Button okButton = new()
+        {
+            Text = "_OK",
+        };
+
+        Dialog d = new()
+        {
+            Title = "Select Packages...",
+            Buttons = [cancelButton, okButton],
+            X = 0,
+            Y = _topY,
+            Width = Dim.Fill(),
+            Height = Dim.Fill(),
+            ButtonAlignment = Dialog.ButtonAlignments.Center,
+        };
+
+        cancelButton.Accept += (s, e) =>
+        {
+            _left = null;
+            _right = null;
+            Application.RequestStop(d);
+        };
+
+        //d.Title = "Select packages for crossing...";
+
+        //d.X = 0;
+        //d.Y = 0;
+        //d.Width = Dim.Fill();
+        //d.Height = Dim.Fill();
+
+        //Label labelSelectedLeft = new()
+        //{
+        //    X = 0,
+        //    Y = 0,
+        //    Width = Dim.Percent(50),
+        //    Text = _left?.Description ?? "Select Left Version",
+        //};
+        //w.Add(labelSelectedLeft);
 
         ListView selectLeft = new()
         {
             X = 0,
-            Y = Pos.Bottom(labelSelectedLeft) + 1,
-            Width = Dim.Percent(50),
-            Height = Dim.Fill(1),
-        };
-        selectLeft.SetSource(_releases);
-        w.Add(selectLeft);
-
-
-        selectLeft.OpenSelectedItem += (s, e) =>
-        {
-            labelSelectedLeft.Text = _releases[e.Item].Description;
-            _left = _releases[e.Item];
-            if ((_right is not null) && (_left is not null))
-            {
-                _state = UiStateCodes.Main;
-                Application.RequestStop();
-            }
-        };
-
-        Label labelSelectedRight = new()
-        {
-            X = Pos.Right(labelSelectedLeft) + 1,
+            //Y = Pos.Bottom(labelSelectedLeft) + 1,
             Y = 0,
             Width = Dim.Percent(50),
-            Text = _right?.Description ?? "Select Right Version",
+            Height = Dim.Fill(1),
+            AllowsMarking = true,
+            AllowsMultipleSelection = false,
         };
-        w.Add(labelSelectedRight);
+        selectLeft.SetSource(_releases);
+        //selectLeft.OpenSelectedItem += (s, e) =>
+        //{
+        //    //labelSelectedLeft.Text = _releases[e.Item].Description;
+        //    //labelSelectedLeft.Draw();
+        //    _left = _releases[e.Item];
+        //    if ((_right is not null) && (_left is not null))
+        //    {
+        //        _state = UiStateCodes.Main;
+        //        Application.RequestStop();
+        //    }
+        //};
+        d.Add(selectLeft);
+
+        //Label labelSelectedRight = new()
+        //{
+        //    X = Pos.Right(labelSelectedLeft) + 1,
+        //    Y = 0,
+        //    Width = Dim.Percent(50),
+        //    Text = _right?.Description ?? "Select Right Version",
+        //};
+        //w.Add(labelSelectedRight);
 
         ListView selectRight = new()
         {
             X = Pos.Right(selectLeft) + 1,
-            Y = Pos.Bottom(labelSelectedRight) + 1,
+            //Y = Pos.Bottom(labelSelectedRight) + 1,
+            Y = 0,
             Width = Dim.Percent(50),
             Height = Dim.Fill(1),
+            AllowsMarking = true,
+            AllowsMultipleSelection = false,
         };
         selectRight.SetSource(_releases);
-        w.Add(selectRight);
+        //selectRight.OpenSelectedItem += (s, e) =>
+        //{
+        //    //labelSelectedRight.Text = _releases[e.Item].Description;
+        //    //labelSelectedRight.Draw();
+        //    _right = _releases[e.Item];
+        //    if ((_right is not null) && (_left is not null))
+        //    {
+        //        _state = UiStateCodes.Main;
+        //        Application.RequestStop();
+        //    }
+        //};
+        d.Add(selectRight);
 
-        selectRight.OpenSelectedItem += (s, e) =>
+        okButton.Accept += (s, e) =>
         {
-            labelSelectedRight.Text = _releases[e.Item].Description;
-            _right = _releases[e.Item];
-            if ((_right is not null) && (_left is not null))
+            for(int i = 0; i < selectLeft.Source.Count; i++)
             {
-                _state = UiStateCodes.Main;
-                Application.RequestStop();
+                if (selectLeft.Source.IsMarked(i))
+                {
+                    _left = _releases[i];
+                    break;
+                }
             }
+
+            for (int i = 0; i < selectRight.Source.Count; i++)
+            {
+                if (selectRight.Source.IsMarked(i))
+                {
+                    _right = _releases[i];
+                    break;
+                }
+            }
+
+            Application.RequestStop(d);
         };
+
+
+        return d;
     }
 
 }
